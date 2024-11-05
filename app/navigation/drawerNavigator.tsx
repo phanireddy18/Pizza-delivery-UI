cdb5ab91693e34f45909e96a8212215fe3bf1405
rbs-qavi <126465739+rbs-qavi@users.noreply.github.com>
2024-11-05 19:03:33 +0530

    checking token for initial screen logout option added

--- app/navigation/drawerNavigator.tsx
+++ app/navigation/drawerNavigator.tsx
@@ -1,18 +1,72 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import PizzaListScreen from '../screen/pizzaListScreen';
 import 'react-native-gesture-handler';
+import { Alert, Text, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { DrawerActions } from '@react-navigation/native';
+import Entypo from 'react-native-vector-icons/Entypo';
+import globalStyles from '../../styles/globalStyle.scss';
 
 const Drawer = createDrawerNavigator();
 
-const DrawerNavigation = () => {
+const DrawerNavigation = ({ navigation }: any) => {
+  const handleLogout = () => {
+    Alert.alert(
+      'Logout',
+      'Are you sure you want to logout?',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        {
+          text: 'Logout',
+          onPress: async () => {
+            // Navigate back to the login screen
+            await AsyncStorage.removeItem('userToken');
+            navigation.dispatch(DrawerActions.closeDrawer());
+            navigation.replace('Login');
+          },
+        },
+      ],
+      { cancelable: true },
+    );
+  };
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
-        <Drawer.Navigator initialRouteName="Home">
-          <Drawer.Screen name="Home" component={PizzaListScreen} />
-        </Drawer.Navigator>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={PizzaListScreen} 
+            options={{
+              // eslint-disable-next-line react/no-unstable-nested-components
+              drawerIcon: () => <Entypo name="home" size={22} color="#000" />,
+              title: 'Home',
+            }}/>
+        <Drawer.Screen
+          name="Logout"
+          options={{
+            // eslint-disable-next-line react/no-unstable-nested-components
+            drawerIcon: () => <Entypo name="log-out" size={22} color="#000" />,
+            title: 'Logout',
+            // eslint-disable-next-line react/no-unstable-nested-components
+            drawerLabel: () => (
+              <View style={globalStyles.logout_container}>
+                <Text style={globalStyles.drawer_text}>Logout</Text>
+              </View>
+
+            ),
+          }}
+          listeners={{
+            drawerItemPress: e => {
+              e.preventDefault(); // Prevent the default navigation
+              handleLogout(); // Invoke the logout function
+            },
+          }}>
+          {() => null}
+        </Drawer.Screen>
+      </Drawer.Navigator>
     </GestureHandlerRootView>
   );
 };
--- app/navigation/mainNavigator.tsx
+++ app/navigation/mainNavigator.tsx
@@ -1,9 +1,10 @@
 // src/navigation/MainNavigator.tsx
-import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import React, { useEffect } from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screen/loginScreen';
 import RegistrationScreen from '../screen/registrationscreen';
 import DrawerNavigation from './drawerNavigator';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export type RootStackParamList = {
   Login: undefined;
@@ -14,22 +15,51 @@ export type RootStackParamList = {
 const Stack = createStackNavigator<RootStackParamList>();
 
 export default function MainNavigator() {
+  const [initialRoute, setInitialRoute] = React.useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    console.log('MainNavigator useEffect triggered');
+    const checkToken = async () => {
+      try {
+        const token = await AsyncStorage.getItem('userToken');
+        console.log('Token from AsyncStorage:', token);
+        if (token) {
+          setInitialRoute('Drawer'); // Set to 'Drawer' if token exists
+          console.log('Navigating to Drawer');
+        } else {
+          setInitialRoute('Login'); // Set to 'Login' if no token exists
+          console.log('Navigating to Login');
+        }
+      } catch (error) {
+        console.error('Error checking token:', error);
+        setInitialRoute('Login'); // Default to Login if error occurs
+      }
+    };
+
+    checkToken();
+  }, []);
+
+  if (initialRoute === undefined) {
+    // Optionally return a loading indicator here while checking the token
+    return null; // Or a loading screen
+  }
+
   return (
-    <Stack.Navigator initialRouteName="Login">
+    <Stack.Navigator  initialRouteName={initialRoute}>
       <Stack.Screen
         name="Login"
         component={LoginScreen}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
       <Stack.Screen
         name="Register"
         component={RegistrationScreen}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
       <Stack.Screen
         name="Drawer"
         component={DrawerNavigation}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   );
